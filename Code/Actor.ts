cc6c681754834587daa0bc870233ac512cddefd8
Unknown <manojlovicsmilos@hotmail.com>
2018-01-27 12:00:43 +0100

    Actor to Sprite

--- Code/Actor.ts
+++ Code/Actor.ts
@@ -2,7 +2,7 @@ export { Actor }
 
 import Engineer from "./Engineer";
 
-class Actor extends Engineer.Tile
+class Actor extends Engineer.Sprite
 {
     private _Target:Actor;
     public constructor(Old?:Actor)
@@ -12,7 +12,6 @@ class Actor extends Engineer.Tile
     }
     public Init()
     {
-        this.Index = -1;
         this.Paint = Engineer.Color.Purple;
         this.Trans.Scale = new Engineer.Vertex(50,50,1);
     }
