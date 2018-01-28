72877f61bdadae62d8145af5c5407c261d9e03c1
Unknown <manojlovicsmilos@hotmail.com>
2018-01-28 11:06:35 +0100

    Effects

--- Code/Effects.ts
+++ Code/Effects.ts
@@ -4,7 +4,22 @@ import Engineer from "./Engineer";
 
 class Effects
 {
+    public static Single:Effects;
+    private _ActiveSplashes:Engineer.Tile[];
+    private _ActiveExplosions:Engineer.Sprite[];
     public constructor()
+    {
+        this.Init();
+    }
+    public Init()
+    {
+        Effects.Single = this;
+    }
+    public GenerateSplash(Location:Engineer.Vertex)
+    {
+
+    }
+    public GenerateExplosion(Location:Engineer.Vertex)
     {
         
     }
